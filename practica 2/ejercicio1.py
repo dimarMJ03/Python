3882a2c59a28ab68c21a3ed87d91eb9ee82055b7
DIMARMJ03 <dimarjminaya99@gmail.com>
2024-01-11 20:54:08 -0300

    calculadora pobre

---
+++ practica 2/ejercicio1.py
@@ -0,0 +1,12 @@
+numero1=int(input("Ingrese el primer número: "))
+numero2=int(input("Ingrese el segundo número: "))
+suma= numero1+numero2
+resta= numero1-numero2
+producto= numero1*numero2
+division= numero1/numero2
+resto= numero1%numero2
+print("La suma es: ", suma)
+print("la resta es: ", resta)
+print("El producto es: ", producto)
+print("La división es: ", division )
+print("El resto es: ", resto)
