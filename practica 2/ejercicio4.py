96cde8f9e9abf43a8aa581877c321ef51bfda0c4
DIMARMJ03 <dimarjminaya99@gmail.com>
2024-01-11 21:30:40 -0300

    concatenar

---
+++ practica 2/ejercicio4.py
@@ -0,0 +1,11 @@
+# la funcion concatena una palabra y un numero
+def concatenar(palabra,numero):
+  cadena=palabra+str(numero)
+  return cadena
+
+#se pide ingresar un string
+tex=input("Ingrese una palabra: ")
+#se pide ingresar un numero
+num=int(input("Ingrese un numero: "))
+union=concatenar(tex,num)
+print(union)
