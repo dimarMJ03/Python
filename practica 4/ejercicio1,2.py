e392420978419f285b7bc3aab9e82b3a8623a8b8
dimarMJ03 <156378173+dimarMJ03@users.noreply.github.com>
2024-04-15 14:57:04 -0300

    Nombres y triplicar

---
+++ practica 4/ejercicio1,2.py
@@ -0,0 +1,7 @@
+#imprime los nombres uno por uno
+nombres=["Maria","Yedimar","Dimarj´","Omar","Melissa"]
+for i in range(len(nombres)):
+ print(nombres[i])
+#triplica la lista con nombre "nombres"
+triplicar= nombres*3
+print(triplicar)
