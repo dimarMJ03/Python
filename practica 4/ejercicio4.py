00d4c247abd034b17d74bcfb99269f37b73eeb95
dimarMJ03 <156378173+dimarMJ03@users.noreply.github.com>
2024-04-15 16:43:48 -0300

    guardar y elevar al cuadrado

---
+++ practica 4/ejercicio4.py
@@ -0,0 +1,7 @@
+#guardar y elevar al cuadrado los numeros de la lista
+numeros=[1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+numeros_cuadrados=[]
+for i in range(1,11):
+  numeros_cuadrados.append(i*i)
+print(numeros_cuadrados)
