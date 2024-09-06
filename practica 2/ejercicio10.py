8311755d9cc5974882aa477dedf4821437b96dac
dimarMJ03 <156378173+dimarMJ03@users.noreply.github.com>
2024-09-06 15:06:21 -0300

    concatenar cadenas

---
+++ practica 2/ejercicio10.py
@@ -0,0 +1,6 @@
+#Pedirle nombre y apellido por separado e imprimir “Apellido, Nombre”.
+#Este proceso se llama concatenar cadenas
+
+nombre=input("Ingrese su nombre: ")
+apellido=input("Ingrese su apellido:")
+print(nombre , apellido)
