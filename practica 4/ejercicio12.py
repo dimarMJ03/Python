75e1bf68ae29eb07b00f89138de62677762d35c0
dimarMJ03 <156378173+dimarMJ03@users.noreply.github.com>
2024-09-11 17:56:12 -0300

    ordenar

---
+++ practica 4/ejercicio12.py
@@ -0,0 +1,11 @@
+#Ejercicio 4
+
+# Lista
+numeros=[343,45,24532,643,76,87]
+# Solo ordena la lista 
+numeros.sort()
+print(numeros)
+# Devuelve una lista ordenada
+orden=sorted(numeros)
+print(orden)
+
